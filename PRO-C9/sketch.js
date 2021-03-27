626a820864d14f9426297d0783ba7cb8d55b0fd9
Sweekrithi Nayak K <sweekrithinayyyak@gmail.com>
2021-03-27 15:38:30 +0530

    done

--- PRO-C9/sketch.js
+++ PRO-C9/sketch.js
@@ -1,14 +1,34 @@
-
+var shipI,ship;
+var backI,back;
 function preload(){
+  backI=loadImage("sea.png");
+  shipI=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
 
 }
 
 function setup(){
   createCanvas(400,400);
+  back=createSprite(200,200);
+  back.x=back.width/2;
+  back.addImage(backI);
+  back.scale=0.25;
+  back.velocityX=2;
+
+  ship=createSprite(200,200,10,10);
+  ship.addAnimation("shipa",shipI);
+  ship.scale=0.45;
+
+  
+
   
 }
 
 function draw() {
   background("blue");
+  if(back.x>400){
+    back.x = 200;
+  }
+  
+  drawSprites();
  
 }
