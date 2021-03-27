2f6be1d214923d59b48da2def61a34b7829d8006
Sweekrithi Nayak K <sweekrithinayyyak@gmail.com>
2021-03-27 15:54:50 +0530

    done

---
+++ PRO-C10/README.md
@@ -0,0 +1,5 @@
+Jaxon visited his best friend's home. He loved the Running Surfers game that his friend was
+playing on his mobile phone. When he returned home, he tried to play the same game on his
+mobile phone, but unfortunately his mobile phone does not support the game. So he decided
+to build a computer game which would be similar to the actual Running Surfers game.
+Can you help Jaxon design the game?
--- PRO-C10/sketch.js
+++ PRO-C10/sketch.js
@@ -1,14 +1,38 @@
-
+var track,trackI;
+var jack,jackI;
+var invi1;;
+var invi2;
 function preload(){
+  trackI=loadImage("path.png");
+  jackI=loadAnimation("Jake1.png","Jake2.png","jake3.png","jake4.PNG","jake5.png");
   //pre-load images
 }
 
 function setup(){
   createCanvas(400,400);
   //create sprites here
+  track=createSprite(200,200)
+  track.addImage(trackI);
+  track.velocityY=2;
+  jack=createSprite(350,350,100,100);
+  jack.addAnimation("runner",jackI);
+
+  invi1=createSprite(0,350,50,100);
+  invi2=createSprite(400,350,50,100);
+
+  invi1.visible=false;
+  invi2.visible=false;
 }
 
 function draw() {
   background(0);
+  if(track.y>400){
+    track.y=200;
+  }
+  jack.x=World.mouseX;
+
+  jack.collide(invi1);
+  jack.collide(invi2);
+  drawSprites();
 
 }
