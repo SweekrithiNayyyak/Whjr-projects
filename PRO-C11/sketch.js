4f33cf5d838218c8179712d3b1b5bae557551118
Sweekrithi Nayak K <sweekrithinayyyak@gmail.com>
2021-04-08 09:40:01 +0530

    done

--- PRO-C11/sketch.js
+++ PRO-C11/sketch.js
@@ -1,9 +1,12 @@
 var garden,rabbit;
 var gardenImg,rabbitImg;
+var appleI, leafI;
 
 function preload(){
   gardenImg = loadImage("garden.png");
   rabbitImg = loadImage("rabbit.png");
+  appleI=loadImage("apple.png");
+  leafI=loadImage("leaf.png")
 }
 
 function setup(){
@@ -23,9 +26,34 @@ rabbit.addImage(rabbitImg);
 
 function draw() {
   background(0);
-  
+  rabbit.x=World.mouseX;
+
+  spawnApples()
   edges= createEdgeSprites();
   rabbit.collide(edges);
 
   drawSprites();
+}
+
+
+function spawnApples(){
+  if(frameCount%80 === 0){
+    num=Math.round(random(1,2));
+    if(num===1){
+      apple=createSprite(Math.round(random(50,350)),10,10,10);
+    apple.velocityY=2;
+    apple.addImage(appleI);
+    apple.scale=0.05;
+    apple.lifetime=150;
+
+    }
+    else{
+      leaf=createSprite(Math.round(random(50,350)),10,10,10);
+    leaf.velocityY=2;
+    leaf.addImage(leafI);
+    leaf.scale=0.05;
+    leaf.lifetime=150;
+    }
+    
+  }
 }
