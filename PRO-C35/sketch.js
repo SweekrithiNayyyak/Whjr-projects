741dd587729f3adb19d2c6bae88ebbeeee8c5507
Sweekrithi Nayak K <sweekrithinayyyak@gmail.com>
2021-05-01 11:15:31 +0530

    complete

--- PRO-C35/sketch.js
+++ PRO-C35/sketch.js
@@ -18,6 +18,9 @@ function setup() {
   balloon.addAnimation("hotAirBalloon",balloonImage1);
   balloon.scale=0.5;
 
+  balloonPos=database.ref('balloon/position')
+  balloonPos.on("value",readpos,showErr)
+
   textSize(20); 
 }
 
@@ -27,19 +30,22 @@ function draw() {
 
   if(keyDown(LEFT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in left direction
+    writepos(-2,0)
   }
   else if(keyDown(RIGHT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in right direction
+    writepos(2,0)
   }
   else if(keyDown(UP_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in up direction
+    writepos(0,-2)
   }
   else if(keyDown(DOWN_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //write code to move air balloon in down direction
+    writepos(0,2)
   }
 
   drawSprites();
@@ -47,4 +53,18 @@ function draw() {
   stroke("white");
   textSize(25);
   text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function writepos(x,y){
+  database.ref('balloon/position').set({
+    x:balloon.x+x,
+    y:balloon.y+y
+  })
+}
+function readpos(data){
+  var pos=data.val();
+  balloon.x = pos.x;
+  balloon.y = pos.y;
+}
+function showErr(){
+  console.log("error");
 }
