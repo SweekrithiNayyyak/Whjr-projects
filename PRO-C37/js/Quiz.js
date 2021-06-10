ca4efef0bf9a9172552e0feb35ea2b3c8a076cd7
Sweekrithi Nayak K <sweekrithinayyyak@gmail.com>
2021-06-10 21:42:17 +0530

    done

--- PRO-C37/js/Quiz.js
+++ PRO-C37/js/Quiz.js
@@ -30,15 +30,35 @@ class Quiz {
 
   play(){
     //write code here to hide question elements
+    question.hide();
 
     //write code to change the background color here
+    background("yellow")
 
     //write code to show a heading for showing the result of Quiz
-
+    textSize(20)
+text("**The person who has answered correct is highlighted with green**",150,20);
     //call getContestantInfo( ) here
+    Contestant.getPlayerInfo()
 
 
     //write condition to check if contestantInfor is not undefined
+    if(allContestants!==undefined){
+      var displaypos=250
+      for(var plr in allContestants){
+        var correctAnswer="2";
+        displaypos+=50;
+        if(allContestants[plr].answer===correctAnswer){
+          fill("green")
+        }
+        else{
+          fill("red")
+        }
+        textSize(25)
+        text(allContestants[plr].name+" : "+allContestants[plr].answer,200,displaypos)
+      }
+
+    }
 
     //write code to add a note here
 
